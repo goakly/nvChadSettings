3675c61f57a8211727a79b345e871d6a26336f27
Daniel <daniel@pop-os.localdomain>
2023-12-09 21:46:21 -0500

    Made delete not overwrite clipboard

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,7 +1,7 @@
 -- local autocmd = vim.api.nvim_create_autocmd
-
+--map("n", "<dd>", '')
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
 --   command = "tabdo wincmd =",
--- })
+vim.api.nvim_set_keymap("n","dd","\"_dd",{ noremap = true })
