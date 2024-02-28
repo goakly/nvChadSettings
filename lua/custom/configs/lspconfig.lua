9622537b06712d32633647a4a3025417e3c46dac
Dan Titch <daniel.titch@metlife.com>
2024-02-28 16:27:36 -0500

    Stupid groovy language support

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -7,11 +7,20 @@ local lspconfig = require "lspconfig"
 local servers = { "html", "cssls", "tsserver", "clangd","jdtls","gopls","groovyls"}
 
 for _, lsp in ipairs(servers) do
+
   lspconfig[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
   }
+
 end
 
+-- Custom setup for groovyls
+lspconfig.groovyls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+
+  cmd = {"java","-jar","/home/daniel/.local/share/nvim/mason/packages/groovy-language-server/build/libs/groovy-language-server-all.jar"}
+}
 -- 
 -- lspconfig.pyright.setup { blabla}
