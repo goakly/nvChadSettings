eda304c5143e1c81442df908a4bb3279afc2f33e
Dan Titch <daniel.titch@metlife.com>
2024-02-28 16:28:06 -0500

    Maps for lsp_lines

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,12 +1,42 @@
 ---@type MappingsTable
 local M = {}
 
+-- local lspLines = require("lsp_lines")
+
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>yl"] = {
+      function()
+        require("lsp_lines").toggle()
+        vim.diagnostic.config { virtual_text = not vim.diagnostic.config().virtual_lines }
+      end,
+      "Toggle lsp_lines",
+      opts = { nowait = true },
+    },
+    ["<leader>yo"] = {
+      function()
+        if vim.diagnostic.config().virtual_lines then
+          require("lsp_lines").toggle()
+        end
+          vim.diagnostic.config { virtual_text = false }
+      end,
+      "Toggle lsp_lines",
+      opts = { nowait = true },
+    },
+    ["<leader>yt"] = {
+      function()
+        if vim.diagnostic.config().virtual_lines then
+          require("lsp_lines").toggle()
+        end
+          vim.diagnostic.config { virtual_text = true }
+      end,
+      "Toggle lsp_lines",
+      opts = { nowait = true },
+    },
   },
   v = {
-    [">"] = { ">gv", "indent"},
+    [">"] = { ">gv", "indent" },
   },
 }
 
