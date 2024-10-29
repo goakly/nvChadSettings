c0f8bfa02df9bc15e08c09509a9bc06716bed348
Daniel <dantitch@gmail.com>
2024-10-29 11:15:59 -0400

    Moved hlargs config to its own file.

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -160,30 +160,7 @@ local plugins = {
     {
       "m-demare/hlargs.nvim",
       init = function()
-        require("hlargs").setup {
-          use_colorpalette = false,
-          sequential_colorpalette = true,
-          colorpalette = {
-            { fg = "#ef9062" },
-            { fg = "#3AC6BE" },
-            { fg = "#35D27F" },
-            { fg = "#EB75D6" },
-            { fg = "#E5D180" },
-            { fg = "#8997F5" },
-            { fg = "#D49DA5" },
-            { fg = "#7FEC35" },
-            { fg = "#F6B223" },
-            { fg = "#F67C1B" },
-            { fg = "#DE9A4E" },
-            { fg = "#BBEA87" },
-            { fg = "#EEF06D" },
-            { fg = "#8FB272" },
-          },
-          highlight = {
-            fg = "#7FEC35",
-            italic = true,
-          },
-        }
+        require "custom.configs.hlargs"
       end,
     },
     -- To make a plugin not be loaded
