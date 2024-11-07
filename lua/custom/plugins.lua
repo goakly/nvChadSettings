7da6f0bbb85c8deb8e663cc325a422797c3c2491
Daniel <dantitch@gmail.com>
2024-11-07 15:35:40 -0500

    Welcome back lazygit

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -3,6 +3,27 @@ local overrides = require "custom.configs.overrides"
 --@type NvPluginSpec[]
 local plugins = {
   -- Tool to analyze key mapping.
+  -- My favorite cmd git tool
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
   {
     "meznaric/key-analyzer.nvim",
     lazy = false,
