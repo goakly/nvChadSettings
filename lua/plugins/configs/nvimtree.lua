e0a98112406428694d1e689bd65536c414e3144c
Daniel <dantitch@gmail.com>
2024-12-17 18:27:56 -0500

    Changed NvimTree git unstaged icon

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -81,7 +81,7 @@ local options = {
           arrow_closed = "",
         },
         git = {
-          unstaged = "✗",
+          unstaged = "",
           staged = "✓",
           unmerged = "",
           renamed = "➜",
