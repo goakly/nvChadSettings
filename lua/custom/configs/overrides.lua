c616669047a9d04b2c7a03f95cf234e3d292586c
Daniel <dantitch@gmail.com>
2024-12-17 16:49:34 -0500

    Added Comment about nvim tree window params

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -52,6 +52,7 @@ M.mason = {
   },
 }
 
+--Change these to change the size of nvim tree window
 local HEIGHT_RATIO = 0.8 -- You can change this
 local WIDTH_RATIO = 0.5 -- You can change this too
 -- git support in nvimtree
