86b513d48db946e74dd20ff66ea3a6900ee3e2cf
Dan Titch <daniel@US8CQSTQ3.metnet.net>
2025-06-03 15:22:50 -0400

    set theme to nightowl

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -5,10 +5,11 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "yoru",
-  theme_toggle = { "yoru", "one_light" },
+  theme = "nightowl",
+  theme_toggle = { "nightowl", "one_light" },
   hl_override = highlights.override,
   hl_add = highlights.add,
+  transparency = false
 }
 
 M.plugins = "custom.plugins"
